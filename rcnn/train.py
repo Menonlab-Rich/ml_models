ba33950d73ba3e961284849d8243bfe5c70b121d
Rich Baird <rich@rbaird.me>
2024-03-29 01:51:40 -0600

    add missing save_annotated_images

--- rcnn/train.py
+++ rcnn/train.py
@@ -1,6 +1,6 @@
 from config import (
     DEVICE, NUM_CLASSES, NUM_EPOCHS, OUT_DIR,
-    VISUALIZE_TRANSFORMED_IMAGES, NUM_WORKERS,
+    VISUALIZE_TRANSFORMED_IMAGES, NUM_WORKERS, SAVE_ANNOTATED_IMAGES
 )
 from model import create_model
 from utils import Averager, SaveBestModel, save_model, save_loss_plot
