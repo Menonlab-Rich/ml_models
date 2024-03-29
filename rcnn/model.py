a6a0446fa320e277f602357264c584af1e51e09a
Rich Baird <rich@rbaird.me>
2024-03-29 03:52:52 -0600

    simplify the backbone due to mem constraints

--- rcnn/model.py
+++ rcnn/model.py
@@ -1,14 +1,16 @@
 import torchvision
-from torchvision.models.detection import fasterrcnn_mobilenet_v3_large_fpn, FastRCNNPredictor
-
-def create_model(num_classes=3):  # Adjusting for 2 target classes + background
-    # Load a pre-trained Faster R-CNN model with a MobileNetV3-Large-FPN backbone
-    model = fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
-
-    # Get the number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+def create_model(num_classes):
     
-    # Replace the pre-trained head with a new one (adjust for your num_classes)
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
+    # load Faster RCNN pre-trained model
+    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+    
+    # get the number of input features 
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # define a new head for the detector with required number of classes
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
     return model
+
+if __name__ == '__main__':
+    model = create_model(3)
+    print(model)
