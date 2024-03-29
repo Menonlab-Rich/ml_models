f13d51fde5196aa2fd465e4300540b7a79fa25e4
Rich Baird <rich@rbaird.me>
2024-03-29 03:35:47 -0600

    simplify the backbone due to mem constraints

--- rcnn/model.py
+++ rcnn/model.py
@@ -1,13 +1,30 @@
 import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-def create_model(num_classes):
-    
-    # load Faster RCNN pre-trained model
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=True)
-    
-    # get the number of input features 
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # define a new head for the detector with required number of classes
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
-    return model
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.backbone_utils import backbone_with_fpn
+from torchvision.models import mobilenet_v3_large
 
+def create_model(num_classes=3):  # Include background in the class count
+    # Load a MobileNetV3-Large model pre-trained on ImageNet
+    mobilenet_backbone = mobilenet_v3_large(pretrained=True)
+    # Remove the classifier, as it's not used in Faster R-CNN
+    backbone = mobilenet_backbone.features
+    # Set the number of output channels in the backbone
+    backbone.out_channels = 960
+    
+    # FPN requires to know the layers of the backbone which will be used
+    return_layers = {'0': 0, '1': 1, '2': 2, '3': 3}  # This is an example, adjust if necessary
+
+    # Create an FPN backbone
+    backbone_fpn = backbone_with_fpn(backbone, 
+                                     out_channels=256,
+                                     returned_layers=[0, 1, 2, 3],
+                                     extra_blocks=None)
+    
+    # Instantiate the Faster R-CNN model using the custom backbone
+    model = FasterRCNN(backbone_fpn, 
+                       num_classes=num_classes,
+                       min_size=800, max_size=1333,
+                       image_mean=[0.485, 0.456, 0.406],
+                       image_std=[0.229, 0.224, 0.225])
+
+    return model
