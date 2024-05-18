7a525cfdc70bd59282fcdb1ead6246358db121fa
Rich Baird <rich@rbaird.me>
2024-05-18 17:35:05 -0600

    update model

--- pix2pix/model.py
+++ pix2pix/model.py
@@ -15,73 +15,54 @@ class _Debug(nn.Module):
             print(f'{k}: {v}')
         return x
 
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=2):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=stride, padding=1, bias=False, padding_mode='reflect'),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
+        
+    def forward(self, x):
+        return self.conv(x)
 
-class Discriminator(nn.Module):
-    """Defines a PatchGAN discriminator"""
 
-    def __init__(
-            self, input_channels, num_filters=64, num_layers=3,
-            norm_layer=nn.BatchNorm2d):
-        """Construct a PatchGAN discriminator
-
-        Parameters:
-            input_channels (int)  -- the number of channels in input images
-            num_filters (int)     -- the number of filters in the first conv layer
-            num_layers (int)      -- the number of conv layers in the discriminator
-            norm_layer            -- normalization layer
-        """
+class Discriminator(nn.Module):        
+    def __init__(self, in_channels=3, out_channels=3, features=[64, 128, 256, 512]):
         super(Discriminator, self).__init__()
-        # no need to use bias as BatchNorm2d has affine parameters
-        if type(norm_layer) == functools.partial:
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
-
-        kernel_size = 4
-        padding_size = 1
-
-        # Initial convolutional layer
-        sequence = [
-            nn.Conv2d(
-                input_channels, num_filters, kernel_size=kernel_size, stride=2,
-                padding=padding_size),
-            nn.LeakyReLU(0.2, inplace=True),
-        ]
-        # Gradually increase the number of filters
-        filter_mult_prev = 1
-
-        for n in range(1, num_layers):
-            filter_mult = min(2 ** n, 8)
-            sequence += [
-                nn.Conv2d(
-                    num_filters * filter_mult_prev, num_filters *
-                    filter_mult, kernel_size=kernel_size, stride=2,
-                    padding=padding_size, bias=use_bias),
-                norm_layer(num_filters * filter_mult),
-                nn.LeakyReLU(0.2, inplace=True),]
-            filter_mult_prev = filter_mult
-
-        # Final layers with no downsampling
-        filter_mult_prev = filter_mult
-        filter_mult = min(2 ** num_layers, 8)
-        sequence += [
-            nn.Conv2d(
-                num_filters * filter_mult_prev, num_filters * filter_mult,
-                kernel_size=kernel_size, stride=1, padding=padding_size,
-                bias=use_bias),
-            norm_layer(num_filters * filter_mult),
-            nn.LeakyReLU(0.2, inplace=True)]
-
-        # Output layer with 1 channel prediction map
-        sequence += [nn.Conv2d(num_filters * filter_mult, 1,
-                               kernel_size=kernel_size, stride=1, padding=padding_size)]
-
-        self.model = nn.Sequential(*sequence)
-
-    def forward(self, input_tensor):
-        """Standard forward."""
-        return self.model(input_tensor)
-
-
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels, features[0], kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
+            nn.LeakyReLU(0.2)
+        )
+        
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(CNNBlock(in_channels, feature, stride=1 if feature == features[-1] else 2))
+            in_channels = feature
+        
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=1, padding=1, padding_mode='reflect'))
+        
+        self.model = nn.Sequential(*layers)
+        
+    def forward(self, x, y):
+        x = torch.cat([x, y], dim=1)
+        x = self.initial(x)
+        x = self.model(x)
+        return x
+         
 # re-export unet model
 Generator = UNet
+
+def test():
+    x = torch.randn((1, 3, 256, 256))
+    y = torch.randn((1, 3, 256, 256))
+    gen = Generator(in_channels=3, out_channels=3)
+    disc = Discriminator(in_channels=6, out_channels=1)
+    assert gen(x).shape == (1, 3, 256, 256), "Generator test failed"
+    assert disc(x, y).shape == (1, 1, 30, 30), "Discriminator test failed"
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test()
