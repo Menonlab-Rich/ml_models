eea5e78d31ddd3a50dc9005d67f46d5d779b337a
Rich Baird <rich@rbaird.me>
2024-05-23 23:10:34 -0600

    Implement weighted MSE

--- encoder/train.py
+++ encoder/train.py
@@ -2,13 +2,15 @@ from base.train import BaseTrainer as Trainer
 import torch
 from model import Autoencoder
 from torch.optim import Adam
-from torch.nn import MSELoss
 from dataset import EncoderDataset, InputLoader
 from os import path
 import utils
 from config import Config
+from torch import nn
 from torch.cuda.amp import GradScaler, autocast
 from torch.utils.data import DataLoader
+from base.loss import WeightedMSELoss
+
 
 class EncoderTrainer(Trainer):
     def __init__(
@@ -47,13 +49,17 @@ class EncoderTrainer(Trainer):
         self.scaler.update()
 
     def step(self, data, *args, **kwargs) -> dict:
-        inputs, targets = data
+        inputs, targets, names, _ = data
+        # If names has 605 and 625, then we need to split the targets and inputs
+        classes = [name[:3] for name in names]
+        classes = [0 if '605' in cls else 1 for cls in classes]
         inputs, targets = inputs.to(
             self.device).float(), targets.to(
             self.device).float()
         with autocast():
             encoded, decoded = self.model(inputs)
-            loss = self.loss_fn(decoded, targets)
+            loss = self.loss_fn(decoded, targets, classes)
+
         return {'loss': loss}
 
     def pre_train(self):
@@ -70,7 +76,8 @@ class EncoderTrainer(Trainer):
             for data in self.val_loader:
                 inputs, targets = data
                 encoded, decoded = self.model(inputs)
-                loss = self.loss_fn(decoded, targets) # Calculate the loss between the decoded and the target
+                # Calculate the loss between the decoded and the target
+                loss = self.loss_fn(decoded, targets)
                 total_loss += loss.item()
         return {'val_loss': total_loss / len(self.val_loader)}
 
@@ -94,11 +101,12 @@ class EncoderTrainer(Trainer):
         # freeze the encoder
         for param in model.encoder.parameters():
             param.requires_grad = False
-        
+
         # pickle and save the encoder
-        torch.save(model.encoder, path.join(self.checkpoint_dir, 'encoder_only_best.pth.tar'))
+        torch.save(model.encoder, path.join(
+            self.checkpoint_dir, 'encoder_only_best.pth.tar'))
         self.plot()
-    
+
     def plot(self):
         from matplotlib import pyplot as plt
         plt.plot(self.losses)
@@ -107,19 +115,29 @@ class EncoderTrainer(Trainer):
         plt.ylabel('MSE Loss')
         plt.savefig(path.join(self.checkpoint_dir, 'val_loss.png'))
 
+
+
+
 if __name__ == '__main__':
     config = Config(file_path=r"D:\CZI_scope\code\ml_models\encoder\config.yml")
-    loss_fn = MSELoss()
+    loss_fn = WeightedMSELoss(weights=torch.tensor(
+        [config.weights['605'], config.weights['625']]), scale=config.loss_scale).to(config.device)
     dataset = EncoderDataset(InputLoader(config.data_dir), config.transform)
-    model = Autoencoder(config.input_channels, config.embedding_dim, rescale_factor=config.rescale)
+    model = Autoencoder(
+        config.input_channels, config.embedding_dim,
+        rescale_factor=config.rescale)
     optimizer = Adam(model.parameters(), lr=config.learning_rate)
-    train_ds, val_ds = dataset.split(0.8)
-    utils.save_data(train_ds, path.join(config.out_dir, 'train_ds.pth.tar')) # Save the training dataset
-    utils.save_data(val_ds, path.join(config.out_dir, 'val_ds.pth.tar')) # Save the validation dataset
-    train_loader = DataLoader(train_ds, batch_size=config.batch_size, shuffle=True)
+    train_ds, val_ds = dataset.split(0.8, return_identifiers=True)
+    # Save the training dataset
+    utils.save_data(train_ds, path.join(config.out_dir, 'train_ds.pth.tar'))
+    # Save the validation dataset
+    utils.save_data(val_ds, path.join(config.out_dir, 'val_ds.pth.tar'))
+    train_loader = DataLoader(
+        train_ds, batch_size=config.batch_size, shuffle=True)
     val_loader = DataLoader(val_ds, batch_size=config.batch_size, shuffle=False)
     trainer = EncoderTrainer(
         model, train_loader, val_loader, optimizer, loss_fn,
-        epochs=config.epochs, device=config.device, checkpoint_dir=config.out_dir)
+        epochs=config.epochs, device=config.device,
+        checkpoint_dir=config.out_dir)
 
     trainer.train()
