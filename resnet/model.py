3b67c9d3a3f616d0c2ab90baf305126f34471653
Rich Baird <rich@rbaird.me>
2024-05-27 01:15:15 -0600

    refactor resnet to use lightning and encoder

--- resnet/model.py
+++ resnet/model.py
@@ -2,8 +2,8 @@ import torchvision.models as models
 from torch import nn
 import torch
 import pytorch_lightning as pl
-from torchmetrics import Accuracy
-from base.loss import with_loss_fn
+from torchmetrics.classification import BinaryAccuracy
+
 
 class SqueezeLaer(nn.Module):
     '''
@@ -11,12 +11,13 @@ class SqueezeLaer(nn.Module):
     In cases where the channel dimension is 1, this layer will remove it
     It will do nothing if the channel dimension is greater than 1
     '''
-    
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-    
+
     def forward(self, x):
         return x.squeeze()
+
 
 def get_model(n_classes, freeze_layers=False, n_channels=3):
     resnet = models.resnet50(weights="DEFAULT")
@@ -29,42 +30,93 @@ def get_model(n_classes, freeze_layers=False, n_channels=3):
 
     if n_channels != 3:
         # Change the first layer to accept n_channels
-        resnet.conv1 = nn.Conv2d(n_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-    
+        resnet.conv1 = nn.Conv2d(
+            n_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
     # Squeeze the output to remove the extra dimension
-    resnet = nn.Sequential(resnet, SqueezeLaer()) 
+    resnet = nn.Sequential(resnet, SqueezeLaer())
 
     # Use DataParallel to split the model across multiple GPUs
     return resnet
 
+
 class ResNet(pl.LightningModule):
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, n_channels=3, encoder=None, lr=1e-3, **kwargs):
         super().__init__()
+        default_hparams = {
+            'n_classes': n_classes,
+            'n_channels': n_channels,
+            'lr': lr,
+        }
+        hparams = {**default_hparams, **kwargs}
+        # remove any callable hyperparameters
+        hparams = {k: v for k, v in hparams.items() if not callable(v)}
+        hparams['encoder'] = encoder.__class__.__name__ if encoder else None
+        self.save_hyperparameters(hparams)
+        self.encoder = encoder.to(self.device) if encoder else None
+        self.accuracy = BinaryAccuracy() # Accuracy metric
         backbone = models.resnet50(weights="DEFAULT")
         n_filters = backbone.fc.in_features
+        if n_channels != 3:
+            # Change the first layer to accept n_channels
+            backbone.conv1 = nn.Conv2d(
+                n_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
         layers = list(backbone.children())[:-1]
+        # Squeeze the output to remove the extra dimension
         self.feature_extractor = nn.Sequential(*layers)
-        self.feature_extractor.eval() # Freeze the feature extractor
+
+        # Freeze the feature extractor except in the case that the first layer is changed
+        # If the first layer is changed, the feature extractor will be frozen from the second layer
+        starting_layer = 1 if n_channels != 3 else 0
+        for layer in self.feature_extractor[starting_layer:]:
+            for param in layer.parameters():
+                param.requires_grad = False
         self.classifier = nn.Linear(n_filters, n_classes)
         self.loss_fn = nn.CrossEntropyLoss()
+        self.encoder = encoder
+        if encoder:
+            self.encoder.to(self.device)  # Move to the device
+            self.encoder.eval()  # Freeze the encoder
+
     def forward(self, x):
         with torch.no_grad():
+            if self.encoder:
+                x, _ = self.encoder(x) # Get the embeddings and ignore the decoder output
             features = self.feature_extractor(x)
-        return self.classifier(features.squeeze())   
-    
+        preds = self.classifier(features.squeeze())
+        return preds.squeeze() # Remove the extra dimension
+
     def _step(self, batch, batch_idx, log_metrics=['loss', 'acc']):
-        x, y = batch
+        x, y, _ = batch
         y_hat = self(x)
         loss = self.loss_fn(y_hat, y)
-        if 'loss' in log_metrics:
-            self.log('loss', loss)
-        if 'acc' in log_metrics:
-            acc = Accuracy()(y_hat, y)
-            self.log('acc', acc)
+        acc = self.accuracy(y_hat, y)
+        metric_w_prefixes = [metric.split('_') for metric in log_metrics]
+        metrics = [mp[0] if len(mp) == 1 else mp[1] for mp in metric_w_prefixes]
+        prefixes = [mp[0] if len(mp) == 2 else '' for mp in metric_w_prefixes]
+        if 'loss' in metrics:
+            for prefix in prefixes:
+                self.log(f'{prefix}_loss', loss)
+        if 'acc' in metrics:
+            for prefix in prefixes:
+                self.log(f'{prefix}_acc', acc)           
         return loss
 
-@with_loss_fn(nn.BCEWithLogitsLoss, weights=[0.25, 0.75])
+    def training_step(self, batch, batch_idx):
+        return self._step(batch, batch_idx)
+    
+    def validation_step(self, batch, batch_idx):
+        return self._step(batch, batch_idx, log_metrics=['val_loss', 'val_acc'])
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+
+
 class BCEResnet(ResNet):
-    def __init__(self):
-        super().__init__(2) # 2 classes for binary classification
-        self.loss_fn = nn.BCEWithLogitsLoss()
+    def __init__(self, weight=None, **kwargs):
+        super().__init__(1, **kwargs)  # 1 classes for binary classification
+        if weight:
+            weight = torch.tensor(weight).to(self.device)
+            self.loss_fn = nn.BCEWithLogitsLoss(weight=weight).to(self.device)
+        else:
+            self.loss_fn = nn.BCEWithLogitsLoss().to(self.device)
--- resnet/train.py
+++ resnet/train.py
@@ -1,11 +1,53 @@
-from pytorch_lightning import Trainer
 from model import BCEResnet
-from dataset import GenericDataModule
+from dataset import ResnetDataModule, InputLoader, TargetLoader
+from config import Config, CONFIG_FILE_PATH
+from pytorch_lightning.loggers import NeptuneLogger
+from os import environ
+from pytorch_lightning import Trainer
+from encoder.model import LitAutoencoder
+
+def load_encoder(ckpt_path: str):
+    encoder = LitAutoencoder.load_from_checkpoint(ckpt_path, strict=False)
+    return encoder
+
+def main(config: Config, n_files: int = None):
+    input_loader = InputLoader(config.data_dir)
+    target_loader = TargetLoader(config.data_dir)
+
+    data_module = ResnetDataModule(
+        input_loader, target_loader, batch_size=config.batch_size,
+        transforms=config.transform)
+
+    model = BCEResnet(
+        weight=None,
+        lr=config.learning_rate,
+        encoder=load_encoder(config.encoder_path),
+        n_channels=config.input_channels,
+    )
+
+    logger = NeptuneLogger(
+        api_key=environ.get("NEPTUNE_API_TOKEN"),  # replace with your own
+        project="richbai90/Resnet",  # format "workspace-name/project-name"
+        tags=["training", "autoencoder", "resnet"],  # optional
+    )
+
+    debug = config.debug
+    if debug['enable']:
+        Trainer(
+            fast_dev_run=debug['fast'],
+            limit_train_batches=debug['train_batches'],
+            limit_val_batches=debug['val_batches'],
+        ).fit(model=model, datamodule=data_module)
+
+    else:
+        Trainer(
+            logger=logger,
+            max_epochs=config.epochs,
+            precision=config.precision,
+            accelerator=config.accelerator,
+        ).fit(model=model, datamodule=data_module)
 
 
-
-def main():
-    data_module = GenericDataModule()
-    model = BCEResnet()
-    trainer = Trainer(max_epochs=10)
-    trainer.fit(model, datamodule=data_module)
+if __name__ == '__main__':
+    config = Config(CONFIG_FILE_PATH)
+    main(config)
