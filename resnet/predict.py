2f5bc97ba80705ff3f905a912721e7ad7c759bda
Rich Baird <rich@rbaird.me>
2024-06-04 10:05:01 -0600

    include logger in predict

--- resnet/predict.py
+++ resnet/predict.py
@@ -6,35 +6,40 @@ from tqdm import tqdm
 from torch import sigmoid, tensor
 import pandas as pd
 from torch.nn import BCEWithLogitsLoss
+from os import environ
 
 
 def load_models(resnet_ckpt_path: str, config: Config):
     from model import ResNet
-    model = ResNet.load_from_checkpoint(resnet_ckpt_path, encoder=None, strict=False)
+    model = ResNet.load_from_checkpoint(
+        resnet_ckpt_path, encoder=None, strict=False)
     test_loader = InputLoader(config.test_dir)
     target_loader = TargetLoader(config.test_dir, config.classes)
-    data_module = ResnetDataModule.load_from_checkpoint(resnet_ckpt_path, test_loaders=(test_loader, target_loader))
+    data_module = ResnetDataModule.load_from_checkpoint(
+        resnet_ckpt_path, test_loaders=(test_loader, target_loader))
     return model, data_module
 
 
-
-
 def main(config: Config):
-    
+
     resnet_ckpt_path = r"D:\CZI_scope\code\ml_models\resnet\checkpoints\resnet-epoch=04-val_accuracy=0.98.ckpt"
     model, data_module = load_models(resnet_ckpt_path, config)
+    logger = NeptuneLogger(
+        api_key=environ.get("NEPTUNE_API_TOKEN"),  # replace with your own
+        project="richbai90/ResnetTest",  # format "workspace-name/project-name"
+        tags=["training", "autoencoder", "resnet"],  # optional
+    )
     trainer_args = {
+        "logger": logger,
         "max_epochs": config.epochs,
         "precision": config.precision,
         "accelerator": config.accelerator,
         "accumulate_grad_batches": 10,
     }
-    
+
     trainer = Trainer(**trainer_args)
     trainer.test(model, datamodule=data_module)
-    
-    
-    
+
 
 if __name__ == '__main__':
     config = Config(r'D:\CZI_scope\code\ml_models\resnet\config.yml')
